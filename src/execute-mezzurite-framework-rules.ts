b379a774f75cb4052c0f5ddff1451492e2396b08
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-02-20 14:52:30 -0800

    Enable linting

--- src/execute-mezzurite-framework-rules.ts
+++ src/execute-mezzurite-framework-rules.ts
@@ -1,39 +1,37 @@
-import {ExtensionConstants} from './constants/extension-constants';
-import {MezzuriteAngular} from './frameworks/mezzurite-angular';
-import {MezzuriteReact} from './frameworks/mezzurite-react';
-import {MezzuriteUtils} from './utils/mezzurite-utils';
+import { ExtensionConstants } from './constants/extension-constants';
+import { MezzuriteAngular } from './frameworks/mezzurite-angular';
+import { MezzuriteUtils } from './utils/mezzurite-utils';
 
 export class ExecuteMezzuriteFrameworkRules {
-    private frameworkName: string;
-    private frameworkversion: number;
-    private filePath: any;
-  
-    constructor(name: string, version: number, filePath: any) {
-      this.frameworkName = name;
-      this.frameworkversion = version;
-      this.filePath = filePath;
-    }
-  
-    async executeRules() {
-        let mezzuriteFramework: any;
-        switch(this.frameworkName){
-            case ExtensionConstants.mezzuriteAngularJs:
-            break;
-            case ExtensionConstants.mezzuriteAngular:
-                mezzuriteFramework = new MezzuriteAngular(this.filePath);
-            break;
-            case ExtensionConstants.mezzuriteReact:
-                mezzuriteFramework = new MezzuriteReact(this.filePath);
-            break;
-            default: console.log('Mezzurite framework name '+ this.frameworkName + ' or version '+ this.frameworkversion +' is invalid!');
-        }
-        if(mezzuriteFramework){
-            return await mezzuriteFramework.executeFrameworkSpecificRules();
-        }
-        return MezzuriteUtils.createOutputObject([], []);
-    }
+  private frameworkName: string;
+  private frameworkversion: number;
+  private filePath: any;
+
+  constructor (name: string, version: number, filePath: any) {
+    this.frameworkName = name;
+    this.frameworkversion = version;
+    this.filePath = filePath;
   }
-  
-  export function ValidateRules(name: string, version: number, filePath: any) {
-    return new ExecuteMezzuriteFrameworkRules(name, version, filePath);
+
+  async executeRules () {
+    let mezzuriteFramework: any;
+    switch (this.frameworkName) {
+      case ExtensionConstants.mezzuriteAngularJs:
+        break;
+      case ExtensionConstants.mezzuriteAngular:
+        mezzuriteFramework = new MezzuriteAngular(this.filePath);
+        break;
+      case ExtensionConstants.mezzuriteReact:
+        break;
+      default: console.log('Mezzurite framework name ' + this.frameworkName + ' or version ' + this.frameworkversion + ' is invalid!');
+    }
+    if (mezzuriteFramework) {
+      return mezzuriteFramework.executeFrameworkSpecificRules();
+    }
+    return MezzuriteUtils.createOutputObject([], []);
   }
+}
+
+export function ValidateRules (name: string, version: number, filePath: any) {
+  return new ExecuteMezzuriteFrameworkRules(name, version, filePath);
+}
