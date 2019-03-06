54330bf89aeb17ce713a22e4a162006013071676
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-03-05 18:21:59 -0800

    Add extractSourceFiles

---
+++ server/src/utilities/extractSourceFiles.test.ts
@@ -0,0 +1,27 @@
+const globby = require('globby');
+import extractSourceFiles from './extractSourceFiles';
+
+describe('extractSourceFiles.ts', () => {
+  beforeAll(() => {
+    globby.sync = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should not error out', () => {
+    extractSourceFiles('test');
+    expect(globby.sync).toHaveBeenLastCalledWith([ `test\\**\\*.+(js|jsx|ts|tsx)`, '!**\\node_modules' ]);
+  });
+
+  it('should handle a Windows style path', () => {
+    extractSourceFiles('file:///c:/some/windows/style/path');
+    expect(globby.sync).toHaveBeenLastCalledWith([ `\\some\\windows\\style\\path\\**\\*.+(js|jsx|ts|tsx)`, '!**\\node_modules' ]);
+  });
+
+  it('should handle a Windows style path with an escaped colon', () => {
+    extractSourceFiles('file:///c%3A/some/windows/style/path');
+    expect(globby.sync).toHaveBeenLastCalledWith([ `\\some\\windows\\style\\path\\**\\*.+(js|jsx|ts|tsx)`, '!**\\node_modules' ]);
+  });
+});
---
+++ server/src/utilities/extractSourceFiles.ts
@@ -0,0 +1,22 @@
+import { sync } from 'globby';
+import { join, normalize } from 'path';
+
+function extractSourceFiles (rootDirectory: string): string[] {
+  let normalized = rootDirectory;
+
+  const colonIndex = normalized.lastIndexOf(':');
+  if (colonIndex > -1) {
+    normalized = normalized.substring(colonIndex + 1);
+  }
+
+  const escapedColonIndex = normalized.lastIndexOf('%3A');
+
+  if (escapedColonIndex > -1) {
+    normalized = normalized.substring(escapedColonIndex + 3);
+  }
+
+  const files = sync([ join(normalize(normalized), '**', '*.+(js|jsx|ts|tsx)'), join('!**', 'node_modules') ]);
+  return files;
+}
+
+export default extractSourceFiles;
