feb4c84e96b6d200cba97fd91083f4e30564c896
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-03-06 12:12:06 -0800

    Assign duplicated expression to variable in isReact

--- server/src/utilities/getComponentType/helpers/isReact.ts
+++ server/src/utilities/getComponentType/helpers/isReact.ts
@@ -5,8 +5,9 @@ function isReact (fileContents: string): boolean {
     const importIndex = fileContents.indexOf(`from 'react'`);
 
     if (importIndex > 0) {
-      hasReactImport = fileContents.substring(0, importIndex).indexOf(' React ') > 0
-        || fileContents.substring(0, importIndex).indexOf(' React,') > 0;
+      const importPortion = fileContents.substring(0, importIndex);
+      hasReactImport = importPortion.indexOf(' React ') > 0
+        || importPortion.indexOf(' React,') > 0;
     }
   }
 
