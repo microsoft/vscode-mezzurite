a2d5b8a72824a6bfa118124d51c3b871da743d88
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-03-06 16:51:19 -0800

    Move getTemplate into separate function

--- server/src/utilities/generateComponent/helpers/generateNgComponent.ts
+++ server/src/utilities/generateComponent/helpers/generateNgComponent.ts
@@ -44,47 +44,10 @@ function containsMezzuriteDirective (filePath: string, sourceClass: ClassDeclara
         const decoratorArgumentParameters = decoratorArgument.getFirstChildByKind(SyntaxKind.SyntaxList).getChildren();
 
         if (decoratorArgumentParameters != null) {
-          let template: string = null;
-          let templateUrl: string = null;
+          const template = getTemplate(decoratorArgumentParameters, filePath);
 
-          decoratorArgumentParameters.forEach((decoratorArgumentParameter: Node) => {
-            if (decoratorArgumentParameter.getKind() === SyntaxKind.PropertyAssignment) {
-              decoratorArgumentParameter.forEachChild((parameter: Node) => {
-                if (parameter != null) {
-                  const parameterText = parameter.getText();
-                  if (parameterText === 'template') {
-                    const templateValue = (parameter.getNextSiblings().find((sibling: Node) => {
-                      return sibling.getKind() === SyntaxKind.NoSubstitutionTemplateLiteral;
-                    }));
-
-                    if (templateValue != null) {
-                      template = templateValue.getText();
-                    }
-                  } else if (parameterText === 'templateUrl') {
-                    const templateUrlValue = (parameter.getNextSiblings().find((sibling: Node) => {
-                      return sibling.getKind() === SyntaxKind.StringLiteral;
-                    }));
-                    if (templateUrlValue != null) {
-                      const fileBase = filePath.substring(0, filePath.lastIndexOf(`\\`));
-                      const fileExtension = templateUrlValue.getText().replace(/'/g, '');
-                      templateUrl = join(fileBase, fileExtension);
-                    }
-                  }
-                }
-              });
-            }
-          });
-
-          if (template != null && template.length > 0) {
-            // TODO: Make this check more robust
+          if (template != null) {
             containsMezzuriteDirective = template.indexOf('mezzurite') > -1;
-          } else if (templateUrl != null && templateUrl.length > 0) {
-            // In the case of both template and templateUrl are found, template is used.
-            try {
-              containsMezzuriteDirective = readFileSync(templateUrl).toString().indexOf('mezzurite') > -1;
-            } catch {
-              console.warn('Invalid path to templateUrl.');
-            }
           }
         }
       }
@@ -92,6 +55,46 @@ function containsMezzuriteDirective (filePath: string, sourceClass: ClassDeclara
   }
 
   return containsMezzuriteDirective;
+}
+
+function getTemplate (decoratorArguments: Node[], filePath: string): string {
+  let template: string = null;
+
+  decoratorArguments.forEach((decoratorArgumentParameter: Node) => {
+    if (decoratorArgumentParameter.getKind() === SyntaxKind.PropertyAssignment) {
+      decoratorArgumentParameter.forEachChild((parameter: Node) => {
+        if (parameter != null) {
+          const parameterText = parameter.getText();
+          if (parameterText === 'template') {
+            const templateValue = (parameter.getNextSiblings().find((sibling: Node) => {
+              return sibling.getKind() === SyntaxKind.NoSubstitutionTemplateLiteral;
+            }));
+
+            if (templateValue != null) {
+              template = templateValue.getText();
+            }
+          } else if (parameterText === 'templateUrl') {
+            const templateUrlValue = (parameter.getNextSiblings().find((sibling: Node) => {
+              return sibling.getKind() === SyntaxKind.StringLiteral;
+            }));
+            if (templateUrlValue != null) {
+              const fileBase = filePath.substring(0, filePath.lastIndexOf(`\\`));
+              const fileExtension = templateUrlValue.getText().replace(/'/g, '');
+              const templateUrl = join(fileBase, fileExtension);
+
+              try {
+                template = readFileSync(templateUrl).toString();
+              } catch {
+                console.warn('Invalid path to templateUrl.');
+              }
+            }
+          }
+        }
+      });
+    }
+  });
+
+  return template;
 }
 
 export default generateNgComponent;
